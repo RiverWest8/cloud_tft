a3c42f50642984a72885c3497abe15bf208143d4
RiverWest8 <50421431+RiverWest8@users.noreply.github.com>
2025-08-12 16:43:40 +0100

    Add files via upload

--- cloud_perm_tft.py
+++ cloud_perm_tft.py
@@ -1431,7 +1431,6 @@ if __name__ == "__main__":
         log_val_interval=10,
         reduce_on_plateau_patience=5,
         reduce_on_plateau_min_lr=1e-5,
-        log_interpretations=False,
     )
     optimizer_params={"weight_decay": WEIGHT_DECAY}
     if isinstance(getattr(tft, "output_layer", None), DualOutputModule):
